21cb84cd7eb2852d08842ba9eea8ecdabdc35eea
lioba00700 <lioba00700@naver.com>
2025-01-06 18:02:42 +0900

    Design :: 채팅-터진 후 페이지 퍼블리싱

---
+++ src/app/boom/page.jsx
@@ -0,0 +1,27 @@
+'use client';
+
+import './page.css';
+import MainButton from '../components/MainButton';
+import { useRouter } from 'next/navigation';
+
+const Boom = () => {
+    const router = useRouter()
+    return(
+        <div className="background">
+        <div className="waiting">
+            <div className="desc">
+                <div className="desc_text">
+                    <p className='desc_bold' style={{marginBottom:0}}>채팅방이</p>
+                    <p className='desc_bold'>터졌어요 💣</p>
+                    <p style={{color:'#a8a8a8'}}>다음에 다시 만나요</p>
+                </div>
+                <div className="mainbtn">
+                    <MainButton color={'#3072FF'} value={'소개보기'} onClick={()=>{router.push('/home')}}/>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Boom;
