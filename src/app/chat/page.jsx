8f10f8293d1f7ce194143307a5e2f270dbaa1450
lioba00700 <lioba00700@naver.com>
2025-01-07 11:15:11 +0900

    Design :: 채팅 페이지 퍼블리싱

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -4,9 +4,13 @@ import { useRouter } from 'next/navigation';
 import './page.css';
 import ChatSomeone from '../components/ChatSomeone';
 import Image from 'next/image';
+import Timer from '../components/Timer';
+import { useState } from 'react';
 
 const Chat = () => {
     const router = useRouter();
+
+    const [message, setMessage] = useState('');
     return(
         <div className="background">
             <div className="chat">
@@ -15,7 +19,7 @@ const Chat = () => {
                         <button onClick={()=>{router.push('/home')}}><Image src="/icons/arrow_left.png" alt="뒤로가기" width={30} height={30}/></button>
                         <p className='chat_title'>테스트 채팅방</p>
                     </div>
-                    <Image src="/images/timer(test).png" alt="타이머(임시)" width={42} height={42}/>
+                    <Timer />
                 </div>
                 <div className="chat_container">
                     <ChatSomeone type={'who'} name={'감자튀김빌런'} message={'감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다'} reaction={2}/>
@@ -26,6 +30,12 @@ const Chat = () => {
                     <ChatSomeone type={'who'} name={'감자튀김빌런'} message={'감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다'} reaction={2}/>
                     <ChatSomeone type={'who'} name={'감자튀김빌런'} message={'감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다'} reaction={2}/>
                     <ChatSomeone type={'who'} name={'감자튀김빌런'} message={'감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다감자튀김 먹고싶다'} reaction={2}/>
+                </div>
+                <div className="chat_input">
+                    <input type="text" placeholder='메시지를 입력해주세요.' onChange={(e)=>{setMessage(e.target.value)}} value={message}/>
+                    <button className="send_img" onClick={()=>{console.log(message); setMessage('')}}>
+                        <Image src="/icons/carbon_send-filled.png" alt="보내기" width={30} height={10} />
+                    </button>
                 </div>
             </div>
         </div>
