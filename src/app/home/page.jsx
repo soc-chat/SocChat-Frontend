23df4060452c0bf7744167f7a06e66d58d0415a8
lioba00700 <lioba00700@naver.com>
2025-01-07 19:58:32 +0900

    Fix :: 화면 잘림 수정

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -14,9 +14,9 @@ const Home = () => {
                 <p style={{color:'#868686'}}>&apos;진짜 랜덤&apos;</p>
                 <p>재밌는 <span style={{color:'#265BCC'}}>시간 제한</span></p>
                 <p>단체 채팅</p>
-                <div className="mainbtn">
-                    <MainButton value={'이메일 입력하고 소식 받기'} color={'#3072ff'} onClick={inputEmail}/>
-                </div>
+            </div>
+            <div className="mainbtn">
+                <MainButton value={'이메일 입력하고 소식 받기'} color={'#3072ff'} onClick={inputEmail}/>
             </div>
           </div>
         </div>
