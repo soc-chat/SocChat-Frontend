0fb0b155ecf9b2a0ca450fa03781e52f0caf358e
lioba00700 <lioba00700@naver.com>
2025-01-08 11:56:04 +0900

    del :: 코드 삭제

--- src/app/components/Timer.jsx
+++ src/app/components/Timer.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import './Timer.css';
 
 const Timer = () => {
-    //라이브러리 버전 문제로 아직 타이머는 구현되지 않았습니다.
     return(
         <div className="timer">
             <CountdownCircleTimer 
