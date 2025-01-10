7217d5c34b7c616aa083f7724098960dc6ef441d
lioba00700 <lioba00700@naver.com>
2025-01-10 09:33:18 +0900

    fix :: 라우팅 정리

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,100 +1,25 @@
-'use client';
+'use client'
 
-import '../app/page.css';
+import './page.css';
+import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-import { useEffect, useState } from 'react';
-const Ping = () => {
-  const [messages, setMessages] = useState([]); // 수신된 메시지 상태
-  const [isConnected, setIsConnected] = useState(false); // WebSocket 연결 상태
-  const [sendmsg, setSendmsg] = useState('');
-  const [Ws, setWS] = useState(null);
+const Splash = () => {
+  const router = useRouter();
 
   useEffect(() => {
-    const ws = new WebSocket(process.env.NEXT_PUBLIC_WEBSOCKET_PORT);
-    setWS(ws);
+    const splashTime = setTimeout(() => {
+      router.push('/home');
+    }, 3000); //3초
 
-    const sendMessage = {
-      channel: 0,
-      content: 'test',
-      userId: '0',
-      type: 'MESSAGE'
-    }
-
-    ws.onopen = () => {
-      console.log('웹소캣 연결 성공');
-      setIsConnected(true);
-
-      const joinMessage = {
-        type: "JOIN",
-        channelId: 1234,
-      };
-      ws.send(JSON.stringify(joinMessage));
-
-      ws.send(JSON.stringify(sendMessage))
-      console.log('전송된 메시지:', sendMessage);
-
-    };
-
-    
-
-    ws.onmessage = (e) => {
-      //const data = JSON.parse(e.data);
-      console.log('Raw message from server:', e.data);
-      const data = setMessages((prev) => [...prev, e.data])
-    //   if (e.type === 'PING') {
-    //     setMessages((prev) => [...prev, e.content]); 
-    //   }
-    };
-
-    ws.onclose = () => {
-      console.log('웹소켓 연결 종료');
-      setIsConnected(false);
-    };
-
-    ws.onerror = (error) => {
-      console.error('에러발생:', error);
-    };
-
-    return () => ws.close();
-  }, []);
-
-
-  const onclickSendMessage = () => {
-    if (Ws && isConnected) {
-
-      
-
-      const sendMessage = {
-        channel: 0,
-        content: sendmsg,
-        userId: '0',
-        type: 'MESSAGE'
-      }
-  
-      Ws.send(JSON.stringify(sendMessage))
-      console.log('전송된 메시지:', sendMessage);
-    } else {
-      console.error('WebSocket 연결되지 않음');
-    }
-  };
-
-  const handleSendMessage = (e) => {
-    setSendmsg(e.target.value);
-  }
-
-
-  return (
-    <div>
-      <h1>Ping Message</h1>
-      <p>{isConnected ? 'Connected' : 'Disconnected'}</p>
-      <input onChange={handleSendMessage}/><button onClick={onclickSendMessage}>전송</button>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index} className='socket_message'>{message}</li> // 수신된 메시지 표시
-        ))}
-      </ul>
+    return () => clearTimeout(splashTime);
+  }, [router]);
+  return(
+    <div className="splash">
+    <Image className="socchat_logo" src="/images/socchat_logo.png" alt="socchat logo"  width={130} height={130}/>
     </div>
-  );
+  )
 }
 
-export default Ping;
+export default Splash;
