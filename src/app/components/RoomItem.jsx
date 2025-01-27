36cbef0f9eb1b52409c0914950639b3d45facabc
lioba00700 <lioba00700@naver.com>
2025-01-28 01:39:54 +0900

    fix :: RoomItem 남은 시간 수정

--- src/app/components/RoomItem.jsx
+++ src/app/components/RoomItem.jsx
@@ -1,15 +1,31 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import './RoomItem.css';
 
 const RoomItem = ({num, name, expireTime}) => {
-    const expire = Number(expireTime);  
+    const [timeRemaining, setTimeRemaining] = useState(0); // expireTime을 상태로 관리
+    const expire = Date.parse(expireTime);
+
+    useEffect(() => {
+        const time = setInterval(() => {
+            const current = new Date().getTime(); // 현재 시간을 밀리초로 얻기
+            
+            console.log('expire: ',expire);
+            console.log('current: ',current);
+            setTimeRemaining(expire-current);
+            console.log(timeRemaining);
+        }, 1000);
+
+        // 컴포넌트가 언마운트될 때 interval 정리
+        return () => clearInterval(time);
+    }, []); // 처음
 
     return(
         <div className="roomItem">
             <p className='num'>{num+1}</p>
             <div className="room_detail">
-                <p style={{color:expire<=10 ? '#FF3030' : '#3072FF', fontSize: '12px'}}>{expire<=10 ? `💣${expire}분 남았어요` : `⌛ ${expire}분 남았어요`}</p>
+                <p style={{color:timeRemaining<=600000 ? '#FF3030' : '#3072FF', fontSize: '12px'}}>{timeRemaining<=600000 ? `💣${Math.floor(timeRemaining/60000)}분 남았어요` : `⌛ ${Math.floor(timeRemaining/60000)}분 남았어요`}</p>
                 <p className="room_name">{name}</p>
             </div>
         </div>
