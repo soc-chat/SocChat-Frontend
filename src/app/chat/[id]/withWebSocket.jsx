7bcc1154d75b1f3539224cda8376313cc7fbc8f8
lioba00700 <lioba00700@naver.com>
2025-01-29 00:14:36 +0900

    fix :: HOC react hook 수정

--- src/app/chat/[id]/withWebSocket.jsx
+++ src/app/chat/[id]/withWebSocket.jsx
@@ -2,104 +2,103 @@ import { useState, useEffect, useRef } from 'react';
 import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
+//리액트 훅은 컴포넌트 내부에서만 호출할 수 있기 때문에 HOC에서 사용하는 것은 React의 규칙에 맞지 않음
+// 따라서 useWebSocket을 따로 만듦
+const useWebSocket = (channelId) => {
+    const clientRef = useRef(null);
+    const [messages, setMessages] = useState([]);
+    const [channelData, setChannelData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const showMessage = (message) => {
+        const parsedMessage = JSON.parse(message.body);
+        setMessages((prev) => [...prev, parsedMessage]);
+    };
+
+    useEffect(() => {
+        if (!channelId) return;
+
+        const socket = new SockJS(process.env.NEXT_PUBLIC_WEBSOCKET_PORT);
+        const client = new Client({
+            webSocketFactory: () => socket,
+            connectHeaders: {},
+            debug: function (str) {
+                console.log(str);
+            },
+            reconnectDelay: 5000,
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000,
+        });
+
+        client.onConnect = function (frame) {
+            console.log("Connected: " + frame);
+            client.subscribe(`/sub/chat/room/${channelId}`, (message) => showMessage(message));
+        };
+
+        client.onDisconnect = () => {
+            console.log('웹소켓 disconnect');
+        };
+
+        client.activate();
+        clientRef.current = client;
+
+        return () => {
+            client.deactivate();
+        };
+    }, [channelId]);
+
+    const showRoomData = async () => {
+        try {
+            const res = await fetch(`https://socchat-api.mya.ong/room/${channelId}`);
+
+            if (res.ok) {
+                const data = await res.json();
+                console.log('data: ', data);
+                setChannelData(data);
+            } else {
+                console.log('error');
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        showRoomData();
+    }, [channelId]);
+
+    const sendMessage = (chatMessage) => {
+        if (clientRef.current) {
+            clientRef.current.publish({
+                destination: '/pub/send',
+                body: JSON.stringify(chatMessage),
+            });
+        }
+    };
+
+    return { messages, sendMessage, channelData, loading };
+};
+
 const WithWebSocket = (ChatPage) => {
-
-    return ({channelId, ...props}) => {
-
-        const clientRef = useRef(null);
-        const [messages, setMessages] = useState([]);
-        const [channelData, setChannelData] = useState(null); //채널 정보 
-        const [loading, setLoading] = useState(true); // 로딩 상태  
-
-        const showMessage = (message) => {
-            const parsedMessage = JSON.parse(message.body); // 메시지 파싱
-            console.log('파싱된 메시지:', parsedMessage);
-
-            setMessages((prev) => [...prev, parsedMessage]); // 상태 업데이트
-        };
-
-        useEffect(()=>{
-            if(!channelId)return;
-
-            const socket = new SockJS(process.env.NEXT_PUBLIC_WEBSOCKET_PORT);
-            const client = new Client({
-                webSocketFactory: () => socket,
-                connectHeaders: {},
-                debug: function (str) {
-                    console.log(str);
-                },
-                reconnectDelay: 5000,
-                heartbeatIncoming: 4000,
-                heartbeatOutgoing: 4000,
-            })
-
-            client.onConnect = function (frame) {
-                console.log("Connected: " + frame);
-                client.subscribe(`/sub/chat/room/${channelId}`, (message) => showMessage(message));
-            };
-        
-
-            client.onDisconnect = () => {
-                console.log('웹소켓 disconnect');
-            }
-            
-            //활성화
-            client.activate();
-            clientRef.current = client;
-
-            return () => { 
-                client.deactivate();
-            }
-        }, [channelId])
-
-        const showRoomData = async () => {
-            try {
-                const res = await fetch(`https://socchat-api.mya.ong/room/${channelId}`);
-                
-                if(res.ok){
-                    const data = await res.json();
-                    console.log('data: ',data);
-                    setChannelData(data);
-                }
-                else{
-                    console.log('error');
-                }
-            }
-            catch(error){
-                console.log(error);
-            }
-            finally{
-                setLoading(false);
-            }
-        };
-
-        useEffect(()=>{
-            showRoomData();
-        }, [])
+    return ({ channelId, ...props }) => {
+        const { messages, sendMessage, channelData, loading } = useWebSocket(channelId);
 
         if (loading) {
             return <div>Loading...</div>; // 로딩 상태 표시
         }
-        
-        const sendMessage = (chatMessage) => {
-            if (clientRef.current) {
-                clientRef.current.publish({
-                  destination: '/pub/send', // 메시지 전송 경로
-                  body: JSON.stringify(chatMessage), // 메시지 내용을 JSON 문자열로 변환
-                });
-            }
-        }
 
-        return(
-            <ChatPage 
+        return (
+            <ChatPage
                 {...props}
                 messages={messages}
                 sendMessage={sendMessage}
                 channelId={channelId}
                 channelData={channelData}
             />
-        )
+        );
     };
-}
+};
 
 export default WithWebSocket;
--- src/app/home/WithFetch.jsx
+++ src/app/home/WithFetch.jsx
@@ -1,43 +1,42 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
+
+const useFetch = () => {
+    const [rooms, setRooms] = useState([]);
+
+    const showRoom = async () => {
+        try {
+            const res = await fetch('https://socchat-api.mya.ong/room');
+            
+            if (res.ok) {
+                const data = await res.json();
+                console.log('data: ', data);
+                setRooms(data);
+            } else {
+                console.log('error');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        showRoom();
+        const interval = setInterval(showRoom, 10000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return rooms;
+};
 
 const WithFetch = (HomePage) => {
     return (props) => {
-        const [rooms, setRooms] = useState([]);
+        const rooms = useFetch(); 
 
-        const showRoom = async () => {
-            try {
-                const res = await fetch('https://socchat-api.mya.ong/room');
-                
-                if(res.ok){
-                    const data = await res.json();
-                    console.log('data: ',data);
-                    setRooms(data);
-                }
-                else{
-                    console.log('error');
-                }
-            }
-            catch(error){
-                console.log(error);
-            }
-        };
-
-        useEffect(() => {
-            showRoom();
-            const interval = setInterval(showRoom, 10000);
-
-            return () => {
-                clearInterval(interval);
-            };
-        }, []);
-
-        return(
-            <HomePage 
-                {...props}
-                rooms={rooms}
-            />
-        )
+        return <HomePage {...props} rooms={rooms} />;
     };
-}
+};
 
 export default WithFetch;
