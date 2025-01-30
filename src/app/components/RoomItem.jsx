3c5a77e509937cd07ee8c987c4afc37b01cdb610
lioba00700 <lioba00700@naver.com>
2025-01-30 22:32:32 +0900

    fix :: 채팅방 열려있으면 바로 이동

--- src/app/components/RoomItem.jsx
+++ src/app/components/RoomItem.jsx
@@ -4,15 +4,19 @@ import { useEffect, useState } from 'react';
 import './RoomItem.css';
 import { useRouter } from 'next/navigation';
 
-const RoomItem = ({num, name, expireTime, channelId}) => {
+const RoomItem = ({num, name, expireTime, channelId, startTime}) => {
     const router = useRouter();
 
     const [timeRemaining, setTimeRemaining] = useState(null); // expireTime을 상태로 관리
+    const [startRoom, setStartRoom] = useState(false);
+
     const expire = Date.parse(expireTime);
+    const start = Date.parse(startTime);
 
     useEffect(() => {
         const time = setInterval(() => {
             const current = new Date().getTime(); // 현재 시간을 밀리초로 얻기
+            if(start<current)setStartRoom(true);
             setTimeRemaining(expire-current);
         }, 1000);
 
@@ -20,9 +24,19 @@ const RoomItem = ({num, name, expireTime, channelId}) => {
         return () => clearInterval(time);
     }, [expire, timeRemaining]); // 처음
 
+    console.log(startRoom);
+
+    const handleGoRoom = () => {
+        if(startRoom){
+            router.push(`/chat/${channelId}`)
+        }
+        else router.push(`/waiting/${channelId}`)
+    }
+
+
     if(!timeRemaining)return
     return(
-        <div className="roomItem" onClick={()=>{router.push(`/waiting/${channelId}`)}}>
+        <div className="roomItem" onClick={handleGoRoom}>
             <p className='num'>{num+1}</p>
             <div className="room_detail">
                 <p style={{color:timeRemaining<=600000 ? '#FF3030' : '#3072FF', fontSize: '12px'}}>{timeRemaining<=600000 ? `💣${Math.floor(timeRemaining/60000)}분 남았어요` : `⌛ ${Math.floor(timeRemaining/60000)}분 남았어요`}</p>
